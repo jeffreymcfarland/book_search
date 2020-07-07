1b13454290c9bc8f1068c149514ba864d423baf9
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-07-06 23:54:48 -0500

    nav bar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NoMatch from './pages/NoMatch';
+import Saved from './pages/Saved';
+import Search from './pages/Search';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
-    <div>
-      <NoMatch />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Search} />
+          <Route exact path='/saved' component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Nav.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import bookImg from '../images/books.png';
+import searchImg from '../images/search.png';
+
+function Nav() {
+
+    const[state, setState] = useState({
+        open: false,
+        width: window.innerWidth
+    })
+
+    return(
+        <nav className='navbar navbar-expand-md navbar-light bg-light'>
+            <Link className='navbar-brand' to='/'>
+                {/* <img src={bookImg} width='35' height='35' className='d-inline-block align-top' alt='logo' loading='lazy'/> */}
+                <span>    ________ <img src={searchImg} width='20' height='20' alt='search icon'/>  Book Search</span>
+            </Link>
+            <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarNav'>
+                <ul className='navbar-nav'>
+                <li className='nav-item active'>
+                    <Link className='nav-link' to='/'>Search <span className='sr-only'>(current)</span></Link>
+                </li>
+                <li className='nav-item'>
+                    <Link className='nav-link' to='/saved'>Saved</Link>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
